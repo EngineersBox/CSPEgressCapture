2791aaca478ac5f7b6845d83ab0f576ccde4b228
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2020-12-13 20:46:03 +1100

    Added crawler and log scraper. Created per-url log matcher and arg parser

---
+++ capture.py
@@ -0,0 +1,98 @@
+from lib.crawler import URLState, AsyncCrawler, Crawler, LimitCrawler, CrawlerConfig
+from lib.logGrabber import ChromeDriver, LogEntryGrabber, LogParser
+import argparse, sys, re
+
+CSP_LOG_TAG = "content-security-policy"
+
+class ProgArgHandler:
+
+    def __init__(self, argv: list):
+        self.addArgs(argv)
+
+    def addArgs(self, argv):
+        self.argv = argv
+
+    def initParser(self):
+        self.description = "Web crawler that pulls console logs and matches against a regex"
+        self.parser = argparse.ArgumentParser(description=self.description)
+        self.parser.add_argument('--domain', '-d', required=True,
+                                help='domain name of website you want to map. i.e. "https://scrapethissite.com"')
+        self.parser.add_argument('--ofile', '-o',
+                                help='define output file to save results of stdout. i.e. "test.txt"')
+        self.parser.add_argument('--limit', '-l',
+                                help='limit search to the given domain instead of the domain derived from the URL. i.e: "github.com"')
+        self.parser.add_argument('--mute', '-m', action="store_true",
+                                help='output only the URLs of pages within the domain that are not broken')
+        self.parser.add_argument('--asynchronous', 'a',
+                                help='run crawler asynchronously at each link')
+
+    def parse(self) -> CrawlerConfig:
+        self.args = self.parser.parse_args()
+        return CrawlerConfig(
+            self.args.domain,
+            self.args.ofile,
+            self.args.limit,
+            self.args.mute,
+            self.args.asynchronous
+        )
+
+class ConsoleLogMatcher:
+
+    def __init__(self, urls: set, to_match: str = CSP_LOG_TAG):
+        self.to_match = to_match
+        self.urls = urls
+        self.logs = set()
+
+    def getLogs(self):
+        for url in self.urls:
+            driver = ChromeDriver(url)
+            grabber = LogEntryGrabber(driver)
+            self.logs = self.logs.union(grabber.slurpLogs())
+
+    def findMatchingLogEntries(self) -> list:
+        logParser = LogParser(self.logs)
+        logParser.setCaptureRegexAsContains(self.to_match)
+        logParser.matchLogs()
+        return logParser.matches
+
+def createCrawler(config: CrawlerConfig):
+    if config.limit is None:
+        if config.asynchronous is None:
+            return Crawler(config)
+        else:
+            return AsyncCrawler(config)
+    else:
+        return LimitCrawler(config)
+
+def collectUrls(config: CrawlerConfig, crawler) -> set:
+    urls = set()
+    if config.asynchronous is not None:
+        urls = urls.union(crawler.url_state.processed_urls)
+        urls = urls.union(crawler.url_state.local_urls)
+        urls = urls.union(crawler.url_state.foreign_urls)
+        urls = urls.difference(urls.broken_urls)
+    else:
+        for vals in crawler.tasks_url_state.values():
+            urls = urls.union(vals.processed_urls)
+            urls = urls.union(vals.local_urls)
+            urls = urls.union(vals.foreign_urls)
+            urls = urls.difference(vals.broken_urls)
+    return urls
+
+if __name__ == "__main__":
+    argHandler = ProgArgHandler()
+    argHandler.addArgs(sys.argv[1:])
+    argHandler.initParser()
+    crawlerConfig = argHandler.parse()
+
+    crawler = createCrawler(crawlerConfig)
+    crawler.crawl()
+
+    urls = collectUrls(crawlerConfig, crawler)
+
+    logMatcher = ConsoleLogMatcher(urls, CSP_LOG_TAG)
+    logMatcher.getLogs()
+    logEntries = logMatcher.findMatchingLogEntries()
+    
+    for entry in logEntries:
+        print(entry)
---
+++ lib/logGrabber.py
@@ -0,0 +1,75 @@
+import logging, re
+from selenium import webdriver
+
+CONTAINS_STR = ".*%s.*%s"
+
+class ChromeDriver:
+
+    def __init__(self, url):
+        self.url = url
+
+    def initWebDriver(self):
+        self.capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+        self.capabilities["goog:loggingPrefs"] = {"browser": "ALL"}
+        self.driver = webdriver.Chrome(desired_capabilities=self.capabilities)
+        self.driver.get(self.url)
+
+class LogEntryGrabber:
+
+    def __init__(self, chrome_driver):
+        self.chrome_driver = chrome_driver
+        self.chrome_driver.initWebDriver()
+
+    def slurpLogs(self):
+        """get log entreies from selenium and add to python logger before returning"""
+        loglevels = { "NOTSET":0 , "DEBUG":10 ,"INFO": 20 , "WARNING":30, "ERROR":40, "SEVERE":40, "CRITICAL":50}
+
+        #initialise a logger
+        browserlog = logging.getLogger("chrome")
+        #get browser logs
+        self.slurped_logs = self.chrome_driver.get_log("browser")
+        for entry in slurped_logs:
+            #convert broswer log to python log format
+            rec = browserlog.makeRecord("%s.%s"%(browserlog.name,entry["source"]),loglevels.get(entry["level"]),".",0,entry["message"],None,None)
+            rec.created = entry["timestamp"] /1000 # log using original timestamp.. us -> ms
+            try:
+                # Add browser log to python log
+                browserlog.handle(rec)
+            except:
+                print(entry)
+        #and return logs incase you want them
+        return self.slurped_logs
+
+class LogParser:
+
+    def __init__(self, logs: list):
+        self.setLogs(logs)
+        self.matches = []
+
+    def setLogs(self, logs: list):
+        self.logs = logs
+
+    def setCaptureRegex(self, regex):
+        self.prog = re.compile(regex)
+
+    def setCaptureRegexAsContains(self, substring, has_newline=False):
+        escaped_substring = re.escape(substring)
+        self.prog = re.compile(CONTAINS_STR.format(
+            escaped_substring, "\n" if has_newline else ""))
+
+    def matchLogs(self):
+        self.matches = []
+        for log in self.logs:
+            self.matches.append(re.match(self.prog, log))
+
+    def readLogsFromFile(self, ifile):
+        with open(ifile, "r") as f:
+            line = f.readline()
+            while line != "":
+                logs.append(line)
+                line = f.readline()
+
+    def writeMatchesToFile(self, ofile):
+        with open(ofile, "w") as f:
+            for match in self.matches:
+                f.writelines(match.group(0))
